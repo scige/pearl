39dd203d27782fba9806ed9578a8c18e65525438
scige <panf06@gmail.com>
2014-06-01 16:02:57 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pearl::Application.config.secret_token = 'c9a210530dd98afbb4d5aebf87968872a9fe4c3a2fe6e07dd88e8e89449e7dcb6d999320d27892a6b5839f5f89fd2697fce3e8b73c137fe676fd3addbe705b0d'
