d905242dcc0c0372f2f9bbc2873b7b92a870c9ec
scige <panf06@gmail.com>
2014-06-14 00:28:55 +0800

    fix plans bug

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,7 @@ class PlansController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @plans = Plan.all
+    @plans = current_user.plans
     @plan = Plan.new
 
     @none_plans = []
