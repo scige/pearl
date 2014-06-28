ae3f9da89f0912546a4a474e25dc65908bfed227
scige <panf06@gmail.com>
2014-06-29 02:33:24 +0800

    modify somethings for better user experience

--- app/helpers/dailies_helper.rb
+++ app/helpers/dailies_helper.rb
@@ -1,2 +1,7 @@
+# coding:utf-8
+
 module DailiesHelper
+  def get_daily_template
+    "==科研进展== (包括：大论文，小论文，文献阅读等)\n1.\n\n==项目和专利== (包括：项目申请，项目开发，专利撰写等)\n1.\n\n==其他== (包括：上课，学习，开会，找工作，感悟，娱乐等)\n1.\n"
+  end
 end
