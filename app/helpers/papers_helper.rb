9e47d10c8fa763a6ba8ae1a05409fb62e96cf2be
scige <panf06@gmail.com>
2014-06-30 00:33:42 +0800

    modify papers and patents view style

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -6,4 +6,14 @@ module PapersHelper
       [Setting.papers.status_publish_string, Setting.papers.status_publish]
     ]
   end
+
+  def get_paper_status_string(status)
+    if status == Setting.papers.status_submit
+      Setting.papers.status_submit_string
+    elsif status == Setting.papers.status_accept
+      Setting.papers.status_accept_string
+    elsif status == Setting.papers.status_publish
+      Setting.papers.status_publish_string
+    end
+  end
 end
--- app/helpers/patents_helper.rb
+++ app/helpers/patents_helper.rb
@@ -9,4 +9,20 @@ module PatentsHelper
       [Setting.patents.status_authorization_string, Setting.patents.status_authorization]
     ]
   end
+
+  def get_patent_status_string(status)
+    if status == Setting.patents.status_plan
+      Setting.patents.status_plan_string
+    elsif status == Setting.patents.status_writing
+      Setting.patents.status_writing_string
+    elsif status == Setting.patents.status_processing
+      Setting.patents.status_processing_string
+    elsif status == Setting.patents.status_publication
+      Setting.patents.status_publication_string
+    elsif status == Setting.patents.status_reality
+      Setting.patents.status_reality_string
+    elsif status == Setting.patents.status_authorization
+      Setting.patents.status_authorization_string
+    end
+  end
 end
